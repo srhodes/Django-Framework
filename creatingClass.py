4af7c9bcf5e1a6cb5b1e01cfb6f1f67332aa123a
srhodes <rhodestephane@comcast.net>
2019-05-07 19:40:12 -0700

    Add files via upload

---
+++ creatingClass.py
@@ -0,0 +1,32 @@
+#MotorBike
+# 3 instances -
+# name 
+
+class MotorBike:
+    def __init__(self, speed):
+        print(speed)
+        self.speed = speed
+        print("MotorBike instance created")
+
+honda = MotorBike(50)
+
+#honda.speed = 50
+
+ducati = MotorBike(250)
+
+#ducati.speed = 250
+
+#ducati_2 = MotorBike()
+
+
+#print(honda)
+#print(ducati)
+
+print(honda.speed)
+print(ducati.speed)
+
+#honda.speed = 150
+#print(honda.speed)
+#print(ducati.speed)
+
+
